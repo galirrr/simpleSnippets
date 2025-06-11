aafbd9fbe422a31650af1c8f87fb8f315ae38483
galirrr <132583666+galirrr@users.noreply.github.com>
2025-06-11 20:38:48 +0530

    Create ytdplCommands.bash

---
+++ ytdplCommands.bash
@@ -0,0 +1,6 @@
+#For bbest audio
+yt-dlp -f bestaudio[ext=m4a]/bestaudio -x --audio-format mp3 https://www.youtube.com/watch?v=4rURry0UFB8
+#For best video
+yt-dlp -f bestvideo+bestaudio/best https://www.youtube.com/watch?v=4rURry0UFB8
+#Sometimes the best flag do not work , check accordingly 
+#Radhe Radhe
